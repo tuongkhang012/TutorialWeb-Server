11e4f59900f714ef0df7f134a1e62c3a34d60d48
AkaneLink <tuongkhang012@gmail.com>
2023-09-15 13:26:36 +0700

    Updated to fit with VueJS

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose, { set } from 'mongoose';
 import logger from 'morgan';
+import cors from 'cors';
 import 'dotenv/config'
 import schoolRouter from "./routes/schoolRoute.js";
 import studentRouter from "./routes/studentRoute.js";
 import equipmentRouter from "./routes/equipmentRoute.js";
 
 const app = express();
-const PORT = process.env.PORT || 6969;
-
+const PORT = process.env.PORT || 8081
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
 app.use(logger('dev'))
-app.set('views', './views')
-app.set('view engine', 'ejs')
 //for using static files
 app.use(express.static('public'));
 
@@ -23,21 +22,6 @@ app.use(express.static('public'));
 app.use('/api', schoolRouter)
 app.use('/api', studentRouter)
 app.use('/api', equipmentRouter)
-
-//index page
-app.get('/', (req, res) => {
-    res.render('pages/index')
-})
-
-//about page
-app.get('/about', (req, res) => {
-    res.render('pages/about')
-})
-
-//random page
-app.get('/test', (req, res) => {
-    res.render('pages/test')
-})
 
 //connect to database
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
